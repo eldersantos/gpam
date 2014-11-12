5bce7de9b213cbc0a44228f762a42d2d07f0e403
Elder Santos <elder.santos@gmail.com>
2014-11-12 17:03:10 -0200

    autoencoder

--- test.py
+++ test.py
@@ -2,27 +2,27 @@ import numpy as np
 from mlp import MLP
 from cross_validation import SCV
 
-in_geral = np.loadtxt("./datasets/breast-cancer.data")
+in_geral = np.loadtxt("./datasets/iris.data")
 
-scv = SCV(in_geral, 10)
+scv = SCV(in_geral, 5)
 t,tt,v, vv = scv.select_fold_combination()
 
 print(t.shape, tt.shape, v.shape, vv.shape)
 hide = np.array([5])
 ann = MLP(t.shape[1], t.shape[1], hide)
 ann.set_erro(0.01)
-ann.set_epochs(1000)
+ann.set_epochs(100)
 ann.validation_set(v,v)
 ann.train_mlp(t, t)
 
 print("Training Error: ", ann.get_validationError())
 print("Validation Error: ", ann.get_trainingError())
 
-#if (ann.get_validationError() < 0.01):
-ann.save_mlp(ann, "./trained_mlp")
+#ann.save_mlp(ann, "./trained_mlp")
 
-ann.plot_learning_curve()
+ann.plot_neurons("Neurons")
 
+#ann.plot_learning_curve()
 
 
 ann = MLP.load_mlp("./trained_mlp.pk1")
