e32e7a3b82cf0da505ec253f90446ecee4505592
Elder Santos <elder.santos@gmail.com>
2014-11-13 00:24:02 -0200

    changes

--- test.py
+++ test.py
@@ -8,9 +8,13 @@ scv = SCV(in_geral, 5)
 t,tt,v, vv = scv.select_fold_combination()
 
 print(t.shape, tt.shape, v.shape, vv.shape)
-hide = np.array([5])
+hide = np.array([2])
+print(hide[0])
 ann = MLP(t.shape[1], t.shape[1], hide)
-ann.set_erro(0.01)
+ann.set_learningRate(0.95)
+ann.set_learningDescent(0.5)
+ann.set_momentum(0.02)
+ann.set_erro(0.005)
 ann.set_epochs(100)
 ann.validation_set(v,v)
 ann.train_mlp(t, t)
@@ -22,7 +26,7 @@ print("Validation Error: ", ann.get_trainingError())
 
 ann.plot_neurons("Neurons")
 
-#ann.plot_learning_curve()
+ann.plot_learning_curve("Neurons")
 
 
 ann = MLP.load_mlp("./trained_mlp.pk1")
